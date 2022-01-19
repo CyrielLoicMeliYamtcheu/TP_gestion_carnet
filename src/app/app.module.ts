6df0e0853b1df1c861375177170f6878e4ac0a57
cyriel loic <meliyamtcheuloic@gmail.com>
2022-01-19 21:23:15 +0100

    second update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { TitreComponent } from './titre/titre.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarnetPageComponent } from './carnet-page/carnet-page.component';
+import { ShowdownModule } from 'ngx-showdown';
+import { FormsModule } from '@angular/forms';
+import { DocumentPageComponent } from './document-page/document-page.component';
+import { DocumentUpdateComponent } from './document-update/document-update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitreComponent,
     NavbarComponent,
-    CarnetPageComponent
+    CarnetPageComponent,
+    DocumentPageComponent,
+    DocumentUpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShowdownModule.forRoot({emoji: true, noHeaderId: true, flavor: 'github'}),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
