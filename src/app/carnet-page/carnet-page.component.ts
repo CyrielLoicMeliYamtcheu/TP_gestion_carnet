f38609efd85bff0b1bbe81b83463f89b855d0e0c
cyriel loic <meliyamtcheuloic@gmail.com>
2022-01-23 21:50:47 +0100

    carnet page

---
+++ src/app/carnet-page/carnet-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carnet-page',
+  templateUrl: './carnet-page.component.html',
+  styleUrls: ['./carnet-page.component.scss']
+})
+export class CarnetPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
