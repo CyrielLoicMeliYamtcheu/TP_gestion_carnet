70f34f7a792d56b4715d4fe11fea6ce5ecbb5ec3
cyriel loic <meliyamtcheuloic@gmail.com>
2022-01-23 21:52:40 +0100

    create carnet

---
+++ src/app/create-carnet/create-carnet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-carnet',
+  templateUrl: './create-carnet.component.html',
+  styleUrls: ['./create-carnet.component.scss']
+})
+export class CreateCarnetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
