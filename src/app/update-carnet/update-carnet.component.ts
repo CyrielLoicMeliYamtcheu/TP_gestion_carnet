2a50a592bdd763b0646bbaf94ee69f9113e665ff
cyriel loic <meliyamtcheuloic@gmail.com>
2022-01-23 21:55:44 +0100

    updated

---
+++ src/app/update-carnet/update-carnet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-carnet',
+  templateUrl: './update-carnet.component.html',
+  styleUrls: ['./update-carnet.component.scss']
+})
+export class UpdateCarnetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
