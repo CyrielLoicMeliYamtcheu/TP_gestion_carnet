467ec818fafeace6deb4db31114c69e0d7fb9e64
MINOU-D <pdjeukui@gmail.com>
2022-01-24 15:44:06 +0100

    update modification

--- src/app/services/carnet.service.ts
+++ src/app/services/carnet.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { elementAt } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -23,11 +24,31 @@ generateId(): number{
   return Date.now();
 }
 
-addCarnet(carnet:any): void{
+addCarnet(carnet:any): void {
   const id = this.generateId();
   carnet.id = id;
   this.listeCarnet.push(carnet);
   localStorage.setItem("carnet", JSON.stringify(this.listeCarnet));
+}
+
+
+getcarnetById(id: number): any {
+  return this.listeCarnet.find((elem: any) => elem.id == id);
+}
+
+updateCarnet(carnet: any): void{
+const id = carnet.id;
+let elem = this.getcarnetById(id);
+console.log("Allooooo");
+console.log(elem);
+if (!elem.id) alert ('Erreur lors de la modification');
+else{
+  let indice = this.listeCarnet.indexOf(elem);
+  if (indice > -1) this.listeCarnet[indice] = carnet;
+  localStorage.setItem("carnet", JSON.stringify(this.listeCarnet));
+  this.getListeCarnet();
+}
+
 }
 
 }
--- src/app/update-carnet/update-carnet.component.ts
+++ src/app/update-carnet/update-carnet.component.ts
@@ -1,3 +1,5 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarnetService } from './../services/carnet.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-carnet.component.scss']
 })
 export class UpdateCarnetComponent implements OnInit {
+  carnets: any;
+  carnet: any;
 
-  constructor() { }
+  constructor(
 
-  ngOnInit(): void {
+    public carnetupdateService: CarnetService,
+    public router: Router,
+    public route: ActivatedRoute
+  ) {
+    this.carnet = {
+      nom: '',
+      description: '',
+    };
   }
-
+  update(): void {
+    this.carnetupdateService.updateCarnet(this.carnet);
+    this.router.navigate(['/carnet']);
+  }
+  reset(): void{
+  this.carnet = { ...this.carnets};
 }
+  ngOnInit(): void {
+    let id: any;
+    this.carnetupdateService.getListeCarnet();
+    this.route.params.subscribe((params) => (id = params['id']));
+    this.carnets = this.carnetupdateService.getcarnetById(id);
+    this.carnet = { ...this.carnets};
+    console.log(this.carnet);
+
+  }
+    }
