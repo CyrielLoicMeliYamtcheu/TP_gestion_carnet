0c2e14a351608467e95f4a68c875ed397710a73e
yantchou <theophile.yantchou@yahoo.fr>
2022-01-25 13:47:14 +0100

    service

---
+++ src/app/searchcarnet.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { SearchcarnetPipe } from './searchcarnet.pipe';
+
+describe('SearchcarnetPipe', () => {
+  it('create an instance', () => {
+    const pipe = new SearchcarnetPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
