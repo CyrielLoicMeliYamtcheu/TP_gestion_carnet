cce6542dc84d71d0d188e65416ec6a1c0d550ae2
cyriel loic <meliyamtcheuloic@gmail.com>
2022-01-26 12:28:51 +0100

    update

--- src/app/create-document/create-document.component.ts
+++ src/app/create-document/create-document.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CarnetService } from '../services/carnet.service';
 
 @Component({
   selector: 'app-create-document',
@@ -7,11 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateDocumentComponent implements OnInit {
 
-  constructor() { }
+  carnets: any;
+  id_carnet: any;
+  constructor(public carnetService: CarnetService, public router: Router, public route: ActivatedRoute) { }
 
+  // format = { nom : "#doc1", categorie:"##cat1"}
   ngOnInit(): void {
 
-
+    let id: any;
+    this.carnetService.getListeCarnet();
+    this.route.params.subscribe((params) => (id = params['id']));
+    this.carnets = this.carnetService.getcarnetById(id);
+    console.log(id);
+    this.id_carnet = id;
   }
 
 }
