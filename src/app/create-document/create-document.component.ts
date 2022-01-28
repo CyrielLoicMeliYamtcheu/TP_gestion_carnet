dc90ddf324e13e8cc97c11c58fa0743481297a11
cyriel loic <meliyamtcheuloic@gmail.com>
2022-01-28 21:59:33 +0100

    update

--- src/app/create-document/create-document.component.ts
+++ src/app/create-document/create-document.component.ts
@@ -5,37 +5,36 @@ import { CarnetService } from '../services/carnet.service';
 @Component({
   selector: 'app-create-document',
   templateUrl: './create-document.component.html',
-  styleUrls: ['./create-document.component.scss']
+  styleUrls: ['./create-document.component.scss'],
 })
 export class CreateDocumentComponent implements OnInit {
-
   carnets: any;
   id_carnet: any;
-  Doc : any;
+  Doc: any;
 
-  constructor(public carnetService: CarnetService, public router: Router, public route: ActivatedRoute) 
-  { 
-     this.Doc = {nom: "", categorie: ""};
-
+  constructor(
+    public carnetService: CarnetService,
+    public router: Router,
+    public route: ActivatedRoute
+  ) {
+    this.Doc = { nom: '', categorie: '' };
   }
-  
-  
-  addDoc(): void{
+
+  addDoc(): void {
     this.carnetService.addDocument(this.Doc, this.carnets);
     this.router.navigate(['/document/' + this.id_carnet]);
-   }
+  }
 
-   reset(): void{
-     this.Doc = {
+  reset(): void {
+    this.Doc = {
       nom: '',
-      categorie: ''
-     };
-   }
+      categorie: '',
+    };
+  }
 
   // format = { nom : "#doc1", categorie:"##cat1"}
 
   ngOnInit(): void {
-
     let id: any;
     this.carnetService.getListeCarnet();
     this.route.params.subscribe((params) => (id = params['id']));
@@ -43,5 +42,4 @@ export class CreateDocumentComponent implements OnInit {
     console.log(id);
     this.id_carnet = id;
   }
-
 }
